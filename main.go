e8295cd7d1a4377b271b2d034a0d50869cc85a55
aaaasahi <asapjorf1017@gmail.com>
2025-02-15 17:15:16 +0900

    基本のHTTPサーバーの実装

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/hello", helloHandler)
+	log.Println("server start at port 8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func helloHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Hello, world!")
 }
