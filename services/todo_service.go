4a0a2524de3d4765f9603557c16b747b509f096c
aaaasahi <asapjorf1017@gmail.com>
2025-03-28 15:18:46 +0900

    残りのService関数の実装

--- services/todo_service.go
+++ services/todo_service.go
@@ -5,9 +5,7 @@ import (
 	"go-todo-app/repositories"
 )
 
-// 指定 ID のTODOを返却
 func GetTodoService(id int) (models.Todo, error) {
-	// TODO : sql.DB 型を手に入れて、変数 db に代入する
 	db, err := connectDB()
 	if err != nil {
 		return models.Todo{}, err
@@ -20,4 +18,64 @@ func GetTodoService(id int) (models.Todo, error) {
 	}
 
 	return todo, nil
+}
+
+func ListTodosService() ([]models.Todo, error) {
+	db, err := connectDB()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	todos, err := repositories.ListTodos(db)
+	if err != nil {
+		return nil, err
+	}
+
+	return todos, nil
+}
+
+func CreateTodoService(todo models.Todo) (models.Todo, error) {
+	db, err := connectDB()
+	if err != nil {
+		return models.Todo{}, err
+	}
+	defer db.Close()
+
+	createdTodo, err := repositories.CreateTodo(db, todo)
+	if err != nil {
+		return models.Todo{}, err
+	}
+
+	return createdTodo, nil
+}
+
+func UpdateTodoService(todo models.Todo) (models.Todo, error) {
+	db, err := connectDB()
+	if err != nil {
+		return models.Todo{}, err
+	}
+	defer db.Close()
+
+	updatedTodo, err := repositories.UpdateTodo(db, todo)
+	if err != nil {
+		return models.Todo{}, err
+	}
+
+	return updatedTodo, nil
+}
+
+func DeleteTodoService(id int) error {
+	db, err := connectDB()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	err = repositories.DeleteTodo(db, id)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
